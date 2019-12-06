b3372b1d0e2dda0bf55e065c8bf7648623259c46
Ryan Torske <rtorske@cnm.edu>
2019-12-05 20:49:42 -0700

    Color picker coding. Basic HTML code reduction and test.

---
+++ script.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+	initColorPicker();
+};
+
+function initColorPicker() {
+	let colorBox = document.getElementById("color-box");
+	let rgb = {
+		red: document.getElementById("red"),
+		green: document.getElementById("green"),
+		blue: document.getElementById("blue")
+	};
+	let colorPickers = document.getElementsByClassName("picker");
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
+}
+
+function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
+	for (let i = 0; i < pickerElements.length; i++) {
+		pickerElements[i].addEventListener('change', () => {
+			console.log("Red value: ", rgb.red.value);
+			let red = rgb.red.value;
+			let green = rgb.green.value;
+			let blue = rgb.blue.value;
+			setBoxBGColor(colorBox, red, green, blue);
+			setDisplayValues(red, green, blue)
+		});
+	}
+}
+
+function setBoxBGColor(colorBox, red, green, blue) {
+	let rgbVal = [red, green, blue].join(',');
+	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+
+function setDisplayValues(red, green, blue){
+	let redVal = document.getElementById("redVal");
+	let greenVal = document.getElementById("greenVal");
+	let blueVal = document.getElementById("blueVal");
+
+	redVal.innerText = red;
+	greenVal.innerText = green;
+	blueVal.innerText = blue;
+}
